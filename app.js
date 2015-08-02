9dca1945fb91a413781810aef2ebd00c26cb21b4
ivan bardon <richarlomenut@MacBook.local>
2015-08-03 01:48:58 +0200

    testing

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ navigator.geolocation.getCurrentPosition(function(pos){
 		});
 		simply.on('singleClick', 'up', function(e){
 			var miss = 	'<a href="//maps.apple.com/?ll='+lat+','+lon+'">Apple Maps</a>'+
-						'\n<a href="//www.google.com/maps/preview/@'+lat+','+lon+',8z';
+						'\n<a href="http://google.com/maps/bylatlng?lat=' + lat + '&lng=' + lng+'"';
 			ajax({
 				method: "POST",
 		        url: "https://api.pushover.net/1/messages.json",
