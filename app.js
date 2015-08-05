f5ae163bf4c1f8bfbbcd6b32dfbd33c43fa55a31
ivan bardon <richarlomenut@MacBook.local>
2015-08-06 00:47:30 +0200

    setting functionality

--- app.js
+++ app.js
@@ -2,43 +2,51 @@
 
 
 // solicitud de las coordenadas
-navigator.geolocation.getCurrentPosition(function(pos){
-	if(pos){
-		var lat = pos.coords.latitude;
-		var lon = pos.coords.longitude;
-		
-		simply.fullscreen(true);
-		simply.style('large');
+var posicionar = function(){
+	navigator.geolocation.getCurrentPosition(function(pos){
+		if(pos){
+			var lat = pos.coords.latitude;
+			var lon = pos.coords.longitude;
+			
+			simply.fullscreen(true);
 
-		
-		simply.text({
-			title:'Mapit',
-			subtitle: 'Arriba mandar tu posicion a alguien\n'+
-				  'Central para guardar tu Posicion\n'+
-				  'Abajo para mandar un S.O.S\n'+
-				  'Boton izquierdo Menu'
-		});
-		simply.on('singleClick', 'down', function(e){
-			var miss = '';
-			ajax({
-				method: "POST",
-		        url: "https://api.pushover.net/1/messages.json",
-		        data: {
-		        	token: "a9UDP7Mvt5GCrvLKhjsqn1arqQ9jMm",
-			        user: "uo7oH8QGgC8CXVizfy4W3m6U2Q3zS1",
-			        device: "iphone",
-			        message: miss
-			    	}
-		    	},
-		        function(data){
-		        	simply.body("Posicion enviada");
-		        	
-		        },
-		        function(data){ simply.body("Fallo al enviar") }
-	        );
-	    });
-	}
-	else{
-		simply.body('Mapit necesita que el navegador web de tu telefono(Safari, Chrome) tenga los permisos de localizacion activados')
-	};
-});
+			
+			simply.text({
+				style: 'large',
+				title:'Mapit',
+				body: 'Arriba mandar tu posicion a alguien\n'+
+					  'Central para mandar un S.O.S\n'+
+					  'Abajo para recibir tu Posicion\n'
+			});
+			simply.on('singleClick', 'down', function(e){
+				var miss = '';
+				ajax({
+					method: "POST",
+			        url: "https://api.pushover.net/1/messages.json",
+			        data: {
+			        	token: "a9UDP7Mvt5GCrvLKhjsqn1arqQ9jMm",
+				        user: "uo7oH8QGgC8CXVizfy4W3m6U2Q3zS1",
+				        device: "iphone",
+				        message: miss
+				    	}
+			    	},
+			        function(data){
+			        	simply.text({
+			        		style:'large',
+			        		title: 'Posicion enviada',
+			        		subtitle: 'Presiona cualquier boton para volver'
+			        	});
+			        	simply.off('singleClick');
+
+			        	
+			        },
+			        function(data){ simply.body("Fallo al enviar") }
+		        );
+		    });
+		}
+		else{
+			simply.body('Mapit necesita que el navegador web de tu telefono(Safari, Chrome) tenga los permisos de localizacion activados')
+		};
+	});
+};
+posicionar();
