a1cc0d12d13e03dff16cae1c6604e324e89592c3
ivanbardon <elsegundopar@gmail.com>
2015-12-07 01:22:04 +0100

    jQuery added +

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+var urlMaker;
+var eventMaker;
 $(document).ready(function(){
-
-	var options = {  urlMaker: $('#urlMakerVal').val(),
-					 eventMaker: $('#eventMakerVal').val()
-				  };
-
 	$('#enviar').click(function () {
+		var urlMaker = $('#urlMakerVal').val();
+		var eventMaker = $('#eventMakerVal').val();
+
+		var options = { urlMaker: urlMaker, eventMaker: eventMaker };
 		document.location = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(options));
 		console.log(JSON.stringify(options));
 	});
