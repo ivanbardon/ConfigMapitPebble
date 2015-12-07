f873ff22c24c32ca03978112b77d72c9b6c62dc1
ivanbardon <elsegundopar@gmail.com>
2015-12-07 01:03:37 +0100

    jQuery added +

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ var eventMaker = $('#eventMakerVal').val();
 var options = { urlMaker: urlMaker, eventMaker: eventMaker };
 
 $('#enviar').click(function () {
-	document.location = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(options))
+	document.location = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(options));
+	console.log(JSON.stringify(options));
 });
